c43255179c4e6b3f0d68f391ab50683348508a45
AbdelRahman <AbdelRahman@AbdelRahman-Lap.wotech-eg.loc>
2025-04-16 10:07:14 +0200

    Apply job

--- job/models.py
+++ job/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.utils.text import slugify
 
 JOB_TYPE = (
     ('Full Time','Full Time'),
     ('Part Time','Part Time'),
 )
+
+def image_upload(instance,filename):
+    imageName , extension = filename.split(".")
+    return 'jobs/%s.%s'%(instance.id,extension)
+
+
 class Job(models.Model):
     title = models.CharField(max_length=100)
     #location
@@ -15,6 +22,14 @@ class Job(models.Model):
     #category Forienkey relationship
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     experience = models.IntegerField(default=1)
+    image = models.ImageField(upload_to = image_upload)
+    
+    
+    slug = models.SlugField(blank=True,null=True)
+    
+    def save(self,*args,**kwargs):
+        self.slug =slugify(self.title)
+        super (Job,self).save(*args, **kwargs)
     
     def __str__(self):
         return self.title
@@ -25,3 +40,16 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
+class Apply(models.Model):
+    
+    job = models.ForeignKey(Job,related_name='apply', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50,blank=True, null=True)
+    email = models.EmailField(max_length=100,blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+    file = models.FileField(upload_to='apply/',blank=True, null=True)
+    coverText = models.TextField(max_length=500,blank=True, null=True)
+    created_at =models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return self.name
